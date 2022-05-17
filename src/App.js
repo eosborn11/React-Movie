2852e0a58b0afa29f88341eb7bbedc91d94257c2
Rikki Crayton <rikkicrayton@Capn-Awesome.localdomain>
2022-05-16 22:17:39 -0400

    Added API call for move titles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ const
 const API_URL = 'http://http://www.omdbapi.com?apikey=1dc19b08';
 
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies(`spiderman`);
+  }, []);
   return (
     <div className="App">
       
